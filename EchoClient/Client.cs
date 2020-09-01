e2ca256e2b209840b5e74b035eb9f7b99c67cd2a
Mads Olsen <mads36g6@edu.easj.dk>
2020-09-01 09:48:53 +0200

    Client oprettet

---
+++ EchoClient/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public void Start()
+        {
+            //Opretter client
+            TcpClient socket = new TcpClient("localhost", 7777);
+
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            String str = sr.ReadLine();
+            Console.WriteLine($"Client into" +  str);
+
+            sw.WriteLine(str);
+            sw.Flush(); // Tømmer buffer
+
+
+
+        }
+    }
+}
---
+++ EchoClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client worker = new Client();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
